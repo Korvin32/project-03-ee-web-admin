6b00db6b0df6f9a5b48643510b891fc347b61d8d
Kostyantyn Zagorodniy <korvin32@yahoo.com>
2016-04-20 11:13:15 +0200

    1 st initial project share

---
+++ src/web/OrderedProductController.java
@@ -0,0 +1,188 @@
+package web;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.ejb.EJB;
+import javax.ejb.EJBException;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import session.OrderedProductFacade;
+import session.OrderedProductManager;
+import web.util.JsfUtil;
+import web.util.JsfUtil.PersistAction;
+import entity.OrderedProduct;
+
+@ManagedBean(name = "orderedProductController")
+@SessionScoped
+public class OrderedProductController implements Serializable {
+
+    @EJB
+    private session.OrderedProductFacade ejbFacade;
+    
+    @EJB
+    private OrderedProductManager ejbManager;
+    
+    private List<OrderedProduct> items = null;
+    
+    private OrderedProduct selected;
+
+    public OrderedProductController() {
+    }
+
+    public OrderedProduct getSelected() {
+        return selected;
+    }
+
+    public void setSelected(OrderedProduct selected) {
+        this.selected = selected;
+    }
+
+    protected void setEmbeddableKeys() {
+        selected.getOrderedProductPK().setProduct(selected.getProduct1().getId());
+        selected.getOrderedProductPK().setCustomerOrder(selected.getCustomerOrder1().getId());
+    }
+
+    protected void initializeEmbeddableKey() {
+        selected.setOrderedProductPK(new entity.OrderedProductPK());
+    }
+
+    private OrderedProductFacade getFacade() {
+        return ejbFacade;
+    }
+
+    private OrderedProductManager getManager() {
+        return ejbManager;
+    }
+    
+    public OrderedProduct prepareCreate() {
+        selected = new OrderedProduct();
+        initializeEmbeddableKey();
+        return selected;
+    }
+
+    public void create() {
+        persist(PersistAction.CREATE, ResourceBundle.getBundle("/Bundle").getString("OrderedProductCreated"));
+        if (!JsfUtil.isValidationFailed()) {
+            items = null;    // Invalidate list of items to trigger re-query.
+        }
+    }
+
+    public void update() {
+        persist(PersistAction.UPDATE, ResourceBundle.getBundle("/Bundle").getString("OrderedProductUpdated"));
+    }
+
+    public void destroy() {
+        persist(PersistAction.DELETE, ResourceBundle.getBundle("/Bundle").getString("OrderedProductDeleted"));
+        if (!JsfUtil.isValidationFailed()) {
+            selected = null; // Remove selection
+            items = null;    // Invalidate list of items to trigger re-query.
+        }
+    }
+
+    public List<OrderedProduct> getItems() {
+        if (items == null) {
+            items = getFacade().findAll();
+        }
+        return items;
+    }
+
+    private void persist(PersistAction persistAction, String successMessage) {
+        if (selected != null) {
+            setEmbeddableKeys();
+            try {
+                if (persistAction != PersistAction.DELETE) {
+                    getFacade().edit(selected);
+                } else {
+                    getFacade().remove(selected);
+                }
+                JsfUtil.addSuccessMessage(successMessage);
+            } catch (EJBException ex) {
+                String msg = "";
+                Throwable cause = ex.getCause();
+                if (cause != null) {
+                    msg = cause.getLocalizedMessage();
+                }
+                if (msg.length() > 0) {
+                    JsfUtil.addErrorMessage(msg);
+                } else {
+                    JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+                JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
+            }
+        }
+    }
+
+    public List<OrderedProduct> getItemsAvailableSelectMany() {
+        return getFacade().findAll();
+    }
+
+    public List<OrderedProduct> getItemsAvailableSelectOne() {
+        return getFacade().findAll();
+    }
+
+    public String doService() {
+        getManager().doDelete();
+        return "/orderedProduct/List.xhtml";
+    }
+    
+    @FacesConverter(forClass = OrderedProduct.class)
+    public static class OrderedProductControllerConverter implements Converter {
+
+        private static final String SEPARATOR = "#";
+        private static final String SEPARATOR_ESCAPED = "\\#";
+
+        @Override
+        public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
+            if (value == null || value.length() == 0) {
+                return null;
+            }
+            OrderedProductController controller = (OrderedProductController) facesContext.getApplication().getELResolver().
+                    getValue(facesContext.getELContext(), null, "orderedProductController");
+            return controller.getFacade().find(getKey(value));
+        }
+
+        entity.OrderedProductPK getKey(String value) {
+            entity.OrderedProductPK key;
+            String values[] = value.split(SEPARATOR_ESCAPED);
+            key = new entity.OrderedProductPK();
+            key.setCustomerOrder(Integer.parseInt(values[0]));
+            key.setProduct(Integer.parseInt(values[1]));
+            return key;
+        }
+
+        String getStringKey(entity.OrderedProductPK value) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(value.getCustomerOrder());
+            sb.append(SEPARATOR);
+            sb.append(value.getProduct());
+            return sb.toString();
+        }
+
+        @Override
+        public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
+            if (object == null) {
+                return null;
+            }
+            if (object instanceof OrderedProduct) {
+                OrderedProduct o = (OrderedProduct) object;
+                return getStringKey(o.getOrderedProductPK());
+            } else {
+                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, "object {0} is of type {1}; expected type: {2}", new Object[]{object, object.getClass().getName(), OrderedProduct.class.getName()});
+                return null;
+            }
+        }
+
+    }
+
+}
